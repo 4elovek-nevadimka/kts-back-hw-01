26e9fdfb025bc95dfd4eed0f51a80e51c62c660f
nevadimka <myregbox@outlook.com>
2023-02-09 09:08:49 +0300

    seconds_to_str solved

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,8 +12,16 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    s = seconds % 60
+    m = seconds // 60 % 60
+    h = seconds // 60 // 60 % 24
+    d = seconds // 24 // 60 // 60
 
+    if d > 0:
+        return f"{d:02d}d{h:02d}h{m:02d}m{s:02d}s"
+    if h > 0:
+        return f"{h:02d}h{m:02d}m{s:02d}s"
+    if m > 0:
+        return f"{m:02d}m{s:02d}s"
 
-
-
+    return f"{s:02d}s"
