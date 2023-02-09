1b069a351177a291fb98ff09bf9516cadc784c8b
nevadimka <myregbox@outlook.com>
2023-02-09 05:57:25 +0300

    shortest / longest word solved

--- tasks/text.py
+++ tasks/text.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import re
 
 __all__ = (
     'find_shortest_longest_word',
@@ -10,4 +11,13 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    raise NotImplementedError
+
+    regex = r'\b\w+\b'
+    words_list = re.findall(regex, text)
+
+    if len(words_list) > 0:
+        longest = max(words_list, key=len)
+        shortest = min(words_list, key=len)
+        return shortest, longest
+
+    return None, None
