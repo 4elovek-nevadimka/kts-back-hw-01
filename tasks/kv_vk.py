ec16f5f39042b705e180066e9ea9c08de093aa74
nevadimka <myregbox@outlook.com>
2023-02-11 05:10:19 +0300

    flip_kv_vk solved

--- tasks/kv_vk.py
+++ tasks/kv_vk.py
@@ -23,7 +23,7 @@ def flip_kv_vk(d: dict[KT, KV]) -> dict[KV, KT]:
         'Москва': 'moscow',
     }
     """
-    raise NotImplementedError
+    return {value: key for key, value in d.items()}
 
 
 def flip_kv_vk_safe(d: dict[KT, KV]) -> dict[KV, list[KT]]:
@@ -39,4 +39,7 @@ def flip_kv_vk_safe(d: dict[KT, KV]) -> dict[KV, list[KT]]:
         '+3': ['Москва', 'Санкт-Петербург'],
     }
     """
-    raise NotImplementedError
+    res = {}
+    for key, val in d.items():
+        res[val] = res.get(val, []) + [key]
+    return res
